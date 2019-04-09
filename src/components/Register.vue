7c2523bbee70a8f928346026854763a7fcfa0ea8
Danny Tran <danny.tran@nab.com.au>
2019-04-09 23:41:31 +1000

    POST and res.send

---
+++ src/components/Register.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <h1>Register</h1>
+    <input type="email" name="email" v-model="email" placeholder="email" />
+    <br />
+    <input
+      type="password"
+      name="password"
+      v-model="password"
+      placeholder="password"
+    />
+    <br />
+    <button @click="register">Register</button>
+  </div>
+</template>
+
+<script lang="ts">
+import AuthenticationService from "@/services/AuthenticationService.js";
+export default {
+  data() {
+    return {
+      email: "",
+      password: ""
+    };
+  },
+  watch: {
+    email(value) {
+      console.log("email has changed", value)
+    }
+  },
+  mounted() {
+    setTimeout(() => {
+      this.email = "hello world";
+    }, 2000);
+  },
+  methods: {
+    async register() {
+      const response = await AuthenticationService.register({
+        email: this.email,
+        password: this.password
+      });
+      console.log(response.data);
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
